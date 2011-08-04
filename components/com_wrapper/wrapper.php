9554d886050036245e2937742f0b46838bc85671
nttuyen <nttuyen_it@yahoo.com>
2011-08-04 23:40:55 +0700

    upgrade: com_wrapper

--- components/com_wrapper/wrapper.php
+++ components/com_wrapper/wrapper.php
@@ -1,18 +1,18 @@
-<?php
-/**
- * @version		$Id: wrapper.php 14276 2010-01-18 14:20:28Z louis $
- * @package		Joomla.Site
- * @subpackage	com_wrapper
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-// no direct access
-defined('_JEXEC') or die;
-
-// Include dependancies
-jimport('joomla.application.component.controller');
-
-$controller = JController::getInstance('Wrapper');
-$controller->execute(JRequest::getCmd('task'));
+<?php
+/**
+ * @version		$Id: wrapper.php 20196 2011-01-09 02:40:25Z ian $
+ * @package		Joomla.Site
+ * @subpackage	com_wrapper
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+// Include dependancies
+jimport('joomla.application.component.controller');
+
+$controller = JController::getInstance('Wrapper');
+$controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
