2b341dd89441417770ecdf55ac4deceefcf41221
nttuyen <nttuyen_it@yahoo.com>
2011-08-04 23:40:40 +0700

    upgrade: com_weblinks

--- components/com_weblinks/weblinks.php
+++ components/com_weblinks/weblinks.php
@@ -1,17 +1,18 @@
-<?php
-/**
- * @version		$Id: weblinks.php 15976 2010-04-10 04:44:23Z hackwar $
- * @package		Joomla.Site
- * @subpackage	com_weblinks
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-// no direct access
-defined('_JEXEC') or die;
-
-jimport('joomla.application.component.controller');
-require_once JPATH_COMPONENT.'/helpers/route.php';
-$controller	= JController::getInstance('Weblinks');
-$controller->execute(JRequest::getCmd('task'));
+<?php
+/**
+ * @version		$Id: weblinks.php 20196 2011-01-09 02:40:25Z ian $
+ * @package		Joomla.Site
+ * @subpackage	com_weblinks
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controller');
+require_once JPATH_COMPONENT.'/helpers/route.php';
+
+$controller	= JController::getInstance('Weblinks');
+$controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
