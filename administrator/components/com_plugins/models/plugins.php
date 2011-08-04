ae0665a79cccfedbd4133605b67b3e7755e6bac4
nttuyen <nttuyen_it@yahoo.com>
2011-08-04 22:29:05 +0700

    upgrade: com_plugins in administrator

--- administrator/components/com_plugins/models/plugins.php
+++ administrator/components/com_plugins/models/plugins.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @version		$Id: plugins.php 16846 2010-05-06 02:45:22Z chdemko $
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @version		$Id: plugins.php 20267 2011-01-11 03:44:44Z eddieajau $
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -20,31 +20,59 @@ jimport('joomla.application.component.modellist');
 class PluginsModelPlugins extends JModelList
 {
 	/**
+	 * Constructor.
+	 *
+	 * @param	array	An optional associative array of configuration settings.
+	 * @see		JController
+	 * @since	1.6
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields'])) {
+			$config['filter_fields'] = array(
+				'extension_id', 'a.extension_id',
+				'name', 'a.name',
+				'folder', 'a.folder',
+				'element', 'a.element',
+				'checked_out', 'a.checked_out',
+				'checked_out_time', 'a.checked_out_time',
+				'state', 'a.state',
+				'enabled', 'a.enabled',
+				'access', 'a.access', 'access_level',
+				'ordering', 'a.ordering',
+				'client_id', 'a.client_id',
+			);
+		}
+
+		parent::__construct($config);
+	}
+
+	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
 	 * @since	1.6
 	 */
-	protected function populateState()
+	protected function populateState($ordering = null, $direction = null)
 	{
 		// Initialise variables.
 		$app = JFactory::getApplication('administrator');
 
 		// Load the filter state.
-		$search = $app->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+		$search = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
 		$this->setState('filter.search', $search);
 
-		$accessId = $app->getUserStateFromRequest($this->context.'.filter.access', 'filter_access', null, 'int');
+		$accessId = $this->getUserStateFromRequest($this->context.'.filter.access', 'filter_access', null, 'int');
 		$this->setState('filter.access', $accessId);
 
-		$state = $app->getUserStateFromRequest($this->context.'.filter.state', 'filter_state', '', 'string');
+		$state = $this->getUserStateFromRequest($this->context.'.filter.state', 'filter_state', '', 'string');
 		$this->setState('filter.state', $state);
 
-		$folder = $app->getUserStateFromRequest($this->context.'.filter.folder', 'filter_folder', null, 'cmd');
+		$folder = $this->getUserStateFromRequest($this->context.'.filter.folder', 'filter_folder', null, 'cmd');
 		$this->setState('filter.folder', $folder);
 
-		$language = $app->getUserStateFromRequest($this->context.'.filter.language', 'filter_language', '');
+		$language = $this->getUserStateFromRequest($this->context.'.filter.language', 'filter_language', '');
 		$this->setState('filter.language', $language);
 
 		// Load the parameters.
@@ -101,7 +129,8 @@ class PluginsModelPlugins extends JModelList
 					}
 				}
 			}
-			JArrayHelper::sortObjects($result,'name', $this->getState('list.direction') == 'desc' ? -1 : 1);
+			$lang = JFactory::getLanguage();
+			JArrayHelper::sortObjects($result,'name', $this->getState('list.direction') == 'desc' ? -1 : 1, true, $lang->getLocale());
 			$total = count($result);
 			$this->cache[$this->getStoreId('getTotal')] = $total;
 			if ($total < $limitstart) {
@@ -186,6 +215,9 @@ class PluginsModelPlugins extends JModelList
 		} else if ($published === '') {
 			$query->where('(a.enabled IN (0, 1))');
 		}
+
+		// Filter by state
+		$query->where('a.state >= 0');
 
 		// Filter by folder.
 		if ($folder = $this->getState('filter.folder')) {
