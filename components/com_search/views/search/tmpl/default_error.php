22c1e97498d86648ff5fce035756dbac64db0b94
nttuyen <nttuyen_it@yahoo.com>
2011-08-04 23:40:22 +0700

    upgrade: com_search

--- components/com_search/views/search/tmpl/default_error.php
+++ components/com_search/views/search/tmpl/default_error.php
@@ -1,18 +1,18 @@
-<?php
-/**
- * @version		$Id: default_error.php 14403 2010-01-26 14:44:45Z dextercowley $
- * @package		Joomla.Site
- * @subpackage	com_search
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-// no direct access
-defined('_JEXEC') or die;
-?>
-
-<?php if($this->error): ?>
-<div class="error">
-			<?php echo $this->escape($this->error); ?>
-</div>
-<?php endif; ?>
+<?php
+/**
+ * @version		$Id: default_error.php 20196 2011-01-09 02:40:25Z ian $
+ * @package		Joomla.Site
+ * @subpackage	com_search
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+
+<?php if($this->error): ?>
+<div class="error">
+			<?php echo $this->escape($this->error); ?>
+</div>
+<?php endif; ?>
